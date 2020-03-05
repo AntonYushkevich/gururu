cd5f37a47db50fef24444346236374b6451bca7a
yushkevich <anton.yushkevich@timvero.com>
2020-03-05 11:44:12 +0300

    code: added submit on press `enter`; styles: improved adaptive; fixed: success arrow position and size

--- src/js/main.js
+++ src/js/main.js
@@ -9,14 +9,19 @@ const mediaQueryPosters = {
     1682: 'background-lg-l.jpg',
 };
 const setElemTitle = el => (title) => el.innerHTML = title;
-const regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
 const setImage = (name) => `assets/images/${name}.svg`;
 const findClosestInt = (obj, width) => Object.keys(obj).reduce(function (prev, curr) {
     return (Math.abs(curr - width) < Math.abs(prev - width) ? curr : prev);
 });
+let inputValue = '';
 
 window.onload = () => {
     focusInput();
+    window.addEventListener('keydown', e => {
+        if (e.keyCode === 13) {
+            handleSubmitClick(e);
+        }
+    });
     addVideoSrc(window.innerWidth);
     const elemWrapper = document.querySelector('.main__block');
     const setTitle = setElemTitle(document.querySelector('.main__block-title'));
@@ -55,27 +60,32 @@ function inputChange({value}) {
     const submitBtn = document.querySelector('.about__submit');
     if (!value.length) {
         submitBtn.src = setImage('arrow-right');
-    } else {
+    } else if (value !== inputValue) {
+        document.querySelector('.about__submit').classList.toggle('about__submit_success', false);
         submitBtn.src = setImage('arrow-right_white');
-    }
-    if (submitBtn.src.includes('arrow-right_error')) {
-        submitBtn.src = setImage('arrow-right_error');
-    }
-    if (document.querySelector('.about__input').className.includes('error')) {
-        document.querySelector('.about__input').className = 'about__input';
+        if (submitBtn.src.includes('arrow-right_error')) {
+            submitBtn.src = setImage('arrow-right_error');
+        }
+        if (document.querySelector('.about__input').className.includes('error')) {
+            document.querySelector('.about__input').className = 'about__input';
+        }
     }
 }
 
 function handleSubmitClick(e) {
     e.preventDefault();
     const inputEl = document.querySelector('#userEmail');
-    const val = inputEl.value;
-    if (val.match(regEmail)) {
+    inputValue = inputEl.value;
+    const regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
+
+    if (inputValue.match(regEmail)) {
         inputEl.blur();
-        return e.target.src = setImage('success')
+        inputValue = '';
+        document.querySelector('.about__submit').classList.toggle('about__submit_success', true);
+        return document.querySelector('.about__submit').src = setImage('success')
         //return submit
     }
-    e.target.src = setImage('arrow-right_error');
+    document.querySelector('.about__submit').src = setImage('arrow-right_error');
     document.querySelector('.about__input').classList.toggle('about_error');
 }
 
